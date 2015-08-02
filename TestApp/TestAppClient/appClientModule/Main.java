60225824d6285acb50d2a7103b7cccc8354584b5
jacqu_000 <jacqu_000@Desktop>
2015-08-02 16:14:29 -0500

    First Commit

---
+++ TestApp/TestAppClient/appClientModule/Main.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+
+public class Main {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "hello");
+		// TODO Auto-generated method stub
+	}
+
+	/* (non-Java-doc)
+	 * @see java.lang.Object#Object()
+	 */
+	public Main() {
+		super();
+		
+	}
+
+}
